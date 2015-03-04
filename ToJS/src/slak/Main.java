1f5adcc1cd994af5aec9ab1b4ae62dab21eff2fd
slak44 <sylviu44@gmail.com>
2015-03-04 19:54:06 +0200

    Can now use file:/// protocol for the input, output and token config Set line wrap, tab size, and monospace font(14)

--- ToJS/src/slak/Main.java
+++ ToJS/src/slak/Main.java
@@ -1,5 +1,6 @@
 package slak;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,13 +19,30 @@ public class Main {
 		JTextArea input = new JTextArea("<Input code here>");
 		JTextArea tokenSpecs = new JTextArea("<Token list here>");
 		JTextArea output = new JTextArea("<Output here>");
+		
+		input.setTabSize(2);
+		tokenSpecs.setTabSize(2);
+		Font fnt = new Font(Font.MONOSPACED, Font.PLAIN, 14);
+		input.setFont(fnt);
+		tokenSpecs.setFont(fnt);
+		output.setFont(fnt);
+		input.setLineWrap(true);
+		tokenSpecs.setLineWrap(true);
+		output.setLineWrap(true);
 		output.setEditable(false);
+		
 		JButton run = new JButton("Transpile");
 		run.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Parser p = new Parser(input.getText()+"\n", tokenSpecs.getText());
-				try {p.parsePseudocode();}
-				finally {output.setText(p.getOutput());}
+				if (input.getText().startsWith("file:///") && tokenSpecs.getText().startsWith("file:///")) {
+					String[] files = input.getText().split("\n");
+					Parser p = new Parser(files[0].substring(8), files[1].substring(8), tokenSpecs.getText().substring(8));
+					output.setText(p.getOutput()+"\nOutputed to file:\n"+files[1].substring(8));
+				} else {
+					Parser p = new Parser(input.getText()+"\n", tokenSpecs.getText());
+					try {p.parsePseudocode();}
+					finally {output.setText(p.getOutput());}
+				}
 			}
 		});
 		
